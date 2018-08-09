580119028280ee4759e21452a23a246bc650962a
rightmeow <jinhedev@gmail.com>
2018-08-09 11:50:19 -0700

    Init

---
+++ app.js
@@ -0,0 +1,38 @@
+var createError = require('http-errors');
+const dotenv = require('dotenv')
+dotenv.config()
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// TODO: custom routes
+
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+})
+
+// error handler
+app.use((err, req, res, next) => {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+    // render the error page
+    res.status(err.status || 500)
+    res.json({
+        status: false,
+        message: err.message || err
+    })
+})
+
+module.exports = app;
