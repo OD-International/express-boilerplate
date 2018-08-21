d8f2e73dfa34528ed66a3306e6365e50b665d53d
rightmeow <jinhedev@gmail.com>
2018-08-21 14:13:48 -0700

    Updates route for smoke testing

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // TODO: custom routes
-app.use('/app', appRoute)
+app.use('/', appRoute)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- app.route.js
+++ app.route.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const Response = require('../helpers/response.helper')
 
-router.get('/', async (req, res, next) => {
+router.get('/ping', async (req, res, next) => {
   const result = Response.formattedContent(true, 'success')
   res.status(200).json(result)
 })
