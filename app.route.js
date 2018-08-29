a0f0234766ba6f4a092e5cd00abe3d9a9d6362e8
rightmeow <jinhedev@gmail.com>
2018-08-29 13:10:17 -0700

    Fixes typo on the ping route

--- app.route.js
+++ app.route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const Response = require('../helpers/response.helper')
 
 router.get('/ping', async (req, res, next) => {
-  const result = Response.formattedContent(true, 'success')
+  const result = Response.formattedContent(true, 'pong')
   res.status(200).json(result)
 })
 
